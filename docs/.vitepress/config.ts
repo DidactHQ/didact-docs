49c0a37887997a74fb9e5bfb3119e6ee1f11d2ef
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-09-03 22:27:11 -0500

    WIP

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -29,56 +29,41 @@ export default defineConfig({
   themeConfig: {
     logo: '/logo.svg',
     nav: [
-        { text: 'Setup', link: '/setup/' },
-        { text: 'Report Slideshows', link: '/report-slideshows/' },
-        { text: 'Dashboard Slideshows', link: '/dashboard-slideshows/'},
-        { text: 'Autolaunch', link: '/autolaunch/' },
-        { text: 'Utilities', link: '/utilities/' }
+        { text: 'Overview', link: '/overview/' },
+        { text: 'Guide', link: '/report-slideshows/' },
+        { text: 'Recipes', link: '/dashboard-slideshows/'},
+        { text: 'API', link: '/autolaunch/' },
     ],
     sidebar: {
-        '/setup/': [
+        '/overview/': [
           {
-            text: 'Setup',
+            text: 'Overview',
             items: [
-              { text: 'Intro', link: '/setup/' },
-              { text: 'What Is Displagent?', link: '/setup/what-is-displagent' },
-              { text: 'Required Credentials', link: '/setup/required-credentials' },
-              { text: 'Power BI Service Account', link: '/setup/power-bi-service-account' },
-              { text: 'Azure App Registration', link: '/setup/azure-app-registration' },
+              { text: 'What Is Didact?', link: '/setup/' },
+              { text: 'Why Build Didact?', link: '/setup/what-is-displagent' },
+              { text: 'Didact vs. Other Tools', link: '/setup/required-credentials' },
+              { text: 'Common Use Cases', link: '/setup/power-bi-service-account' },
+              { text: 'Licensing', link: '/setup/azure-app-registration' },
             ]
-          }
-        ],
-        '/report-slideshows/': [
+          },
           {
-            text: 'Report Slideshows',
+            text: 'Architecture',
             items: [
-              { text: 'Intro', link: '/report-slideshows/' },
-              { text: 'The Report Slideshows Table', link: '/report-slideshows/report-slideshows-table' },
-              { text: 'Create a Report Slideshow', link: '/report-slideshows/create-report-slideshow' },
-              { text: 'Build a Report Slideshow', link: '/report-slideshows/build-report-slideshow' },
-              { text: 'Launch a Report Slideshow', link: '/report-slideshows/launch-report-slideshow' },
-              { text: 'Delete a Report Slideshow', link: '/report-slideshows/delete-report-slideshow' }
+              { text: 'What Is Didact?', link: '/setup/' },
+              { text: 'Why Build Didact?', link: '/setup/what-is-displagent' },
+              { text: 'Didact vs. Other Tools', link: '/setup/required-credentials' },
+              { text: 'Common Use Cases', link: '/setup/power-bi-service-account' },
+              { text: 'Licensing', link: '/setup/azure-app-registration' },
             ]
-          }
-        ],
-        '/dashboard-slideshows/': [
+          },
           {
-            text: 'Dashboard Slideshows',
+            text: 'Core Concepts',
             items: [
-              { text: 'Intro', link: '/dashboard-slideshows/' },
-              { text: 'The Dashboard Slideshows Table', link: '/dashboard-slideshows/dashboard-slideshows-table' },
-              { text: 'Create a Dashboard Slideshow', link: '/dashboard-slideshows/create-dashboard-slideshow' },
-              { text: 'Launch a Dashboard Slideshow', link: '/dashboard-slideshows/launch-dashboard-slideshow' },
-              { text: 'Delete a Dashboard Slideshow', link: '/dashboard-slideshows/delete-dashboard-slideshow' }
-            ]
-          }
-        ],
-        '/autolaunch/': [
-          {
-            text: 'Autolaunch',
-            items: [
-              { text: 'Intro', link: '/autolaunch/' },
-              { text: 'Autolaunch Settings', link: '/autolaunch/autolaunch-settings' }
+              { text: 'What Is Didact?', link: '/setup/' },
+              { text: 'Why Build Didact?', link: '/setup/what-is-displagent' },
+              { text: 'Didact vs. Other Tools', link: '/setup/required-credentials' },
+              { text: 'Common Use Cases', link: '/setup/power-bi-service-account' },
+              { text: 'Licensing', link: '/setup/azure-app-registration' },
             ]
           }
         ]
---
+++ docs/overview/index.md
@@ -0,0 +1,4 @@
+---
+title: Overview
+description: An introduction to the Didact platform including what it is, why it was built, and its licensing.
+---
