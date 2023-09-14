723524f1ce9dc6916df0715d432ae2ac03034e91
Daniel Miradakis <dmiradakis@owensgrp.com>
2023-09-14 13:05:07 -0500

    Fixed VitePress footer spelling

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -103,7 +103,7 @@ export default defineConfig({
       pattern: 'https://github.com/DidactHQ/didact-docs/edit/main/docs/:path'
     },
     footer: {
-      message: 'Proudly powered by <a style="color: #f97316;" target="_blank" href="https://www.vitepress.dev">Vitepress</a>',
+      message: 'Proudly powered by <a style="color: #f97316;" target="_blank" href="https://www.vitepress.dev">VitePress</a>',
       copyright: 'Copyright © 2023-present Daniel Miradakis'
     }
   },
